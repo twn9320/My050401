3c9c9f73da63a4050894261a4960c565e0737e35
twn9320 <twn9320@gmail.com>
2017-05-04 09:10:04 +0800

    PendingIntent

---
+++ app/src/main/java/com/example/yvtc/my050401/DetailActivity.java
@@ -0,0 +1,13 @@
+package com.example.yvtc.my050401;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
--- app/src/main/java/com/example/yvtc/my050401/MainActivity.java
+++ app/src/main/java/com/example/yvtc/my050401/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.yvtc.my050401;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     NotificationManager manager;
+    final int NOTIFICATION_ID = 321;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,11 +19,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void click1 (View v){
+        Intent it = new Intent(MainActivity.this,DetailActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(MainActivity.this,123,it,PendingIntent.FLAG_UPDATE_CURRENT);
         Notification.Builder builder = new Notification.Builder(MainActivity.this);
         builder.setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle("這是通知")
-                .setContentText("這裡是內容");
+                .setContentText("這裡是內容")
+                .setContentIntent(pi)
+                .setAutoCancel(true);
         Notification notification = builder.build();
-        manager.notify(321,notification);
+        manager.notify(NOTIFICATION_ID,notification);
     }
 }
