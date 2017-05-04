2ec31c8f85d01b1aeb23c96081f124f3f0268555
twn9320 <twn9320@gmail.com>
2017-05-04 14:07:13 +0800

    連結資料庫(溫度)

--- app/src/main/java/com/example/yvtc/my050401/MyService.java
+++ app/src/main/java/com/example/yvtc/my050401/MyService.java
@@ -11,12 +11,19 @@ import android.os.IBinder;
 import android.support.annotation.IntDef;
 import android.util.Log;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class MyService extends Service {
     Handler handler = new Handler();
     int count;
     Context context;
     NotificationManager manager;
     final int NOTIFICATION_ID =567;
+    final int NOTIFICATION_TEMP_ID =789;
     public MyService() {
     }
 
@@ -69,8 +76,36 @@ public class MyService extends Service {
         context = getApplicationContext();
         manager = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
         handler.post(showTime);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("temp");
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                int temp = Integer.valueOf(value);
+                Log.d("SER1","Temp:"+temp);
+                if(temp >=40){
+                    Intent it = new Intent(context, DetailActivity.class);
+                    String msg = "溫度過高!! , 目前 "+temp +"度!";
+                    it.putExtra("msg", msg);
+                    PendingIntent pi = PendingIntent.getActivity(context, 123, it, PendingIntent.FLAG_UPDATE_CURRENT);
+                    Notification.Builder builder = new Notification.Builder(MyService.this);
+                    builder.setSmallIcon(R.mipmap.ic_launcher)
+                            .setContentTitle("這是高溫警報")
+                            .setContentText(msg)
+                            .setContentIntent(pi)
+                            .setAutoCancel(true);
+                    Notification notification = builder.build();
+                    manager.notify(NOTIFICATION_TEMP_ID, notification);
+                }
+            }
 
-        return super.onStartCommand(intent, flags, startId);
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        return START_NOT_STICKY;
     }
 
     @Override
